1c556661716b227b9d9b08039243b468dfaa7f44
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-06-14 06:53:47 +0100

    adding price to the parameters

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.24;
 
 contract TokenSale {
     uint256 public ratio;
+    uint256 public price;
     
-    constructor(uint256 _ratio){
+    constructor(uint256 _ratio, uint256 _price){
         ratio = _ratio;
+        price = _price;
     }
     
 }
--- test/TokenSale.ts
+++ test/TokenSale.ts
@@ -2,16 +2,19 @@ import { expect } from "chai";
 import { viem } from "hardhat";
 
 const TEST_RATIO = 10n;
+const TEST_PRICE = 5n;
 
 describe("NFT Shop", async () => {
     describe("When the Shop contract is deployed", async () => {
         it("defines the ratio as provided in parameters", async () => {
-            const tokenSaleContract = await viem.deployContract("TokenSale", [TEST_RATIO]);
+            const tokenSaleContract = await viem.deployContract("TokenSale", [TEST_RATIO, TEST_PRICE]);
             const ratio = await tokenSaleContract.read.ratio();
             expect (ratio).eq(TEST_RATIO);
         })
         it("defines the price as provided in parameters", async () => {
-            throw new Error("Not implemented");
+            const tokenSaleContract = await viem.deployContract("TokenSale", [TEST_RATIO, TEST_PRICE]);
+            const ratio = await tokenSaleContract.read.price();
+            expect (ratio).eq(TEST_PRICE);
         });
         it("uses a valid ERC20 as payment token", async () => {
             throw new Error("Not implemented");
